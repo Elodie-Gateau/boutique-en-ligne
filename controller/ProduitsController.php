cd807d5a63ae1602106f9b1242f3c1d2589de4a9
Elodie Gateau <elodie.gateau.raffeneau@gmail.com>
2025-07-10 12:07:19 +0200

    Ajout du formulaire ajout produit espace admin

--- controller/ProduitsController.php
+++ controller/ProduitsController.php
@@ -4,6 +4,16 @@ class ProduitsController
 
     public function addProduct()
     {
-        ProduitsRepository::addProduct();
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $product = new Produit;
+            $product->setNom($_POST['nom']);
+            $product->setPrix($_POST['prix_unitaire']);
+            $product->setDescription($_POST['description']);
+            $product->setType($_POST['type']);
+            $product->setUrl_img($_POST['url_img']);
+        }
+
+
+        ProduitsRepository::addProduct($product);
     }
 }
