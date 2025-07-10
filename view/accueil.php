23789b648db222da1333ff9f18273cbba30b5212
FabThms <thms.fab44@gmail.com>
2025-07-10 14:25:30 +0200

    ajout du style css

--- view/accueil.php
+++ view/accueil.php
@@ -1,9 +1,1 @@
-<h1 class="titre">
-    <?php
-    if (isset($_SESSION['prenom'])) {
-        echo 'Bienvenue, ' . htmlspecialchars($_SESSION['prenom']) . ' !';
-    } else {
-        echo 'Bienvenue sur la boutique !';
-    }
-    ?>
-</h1>
+require_once 'include/header.php';
--- view/layout/header.php
+++ view/layout/header.php
@@ -1,14 +1,24 @@
 <header>
-
-    <nav>
-        <a href="index.php?page=accueil">Accueil</a> |
-        <?php if (!isset($_SESSION['email'])): ?>
-            <a href="index.php?page=register">S'inscrire</a> |
-            <a href="index.php?page=connexion">Se connecter</a> |
-        <?php else: ?>
-            <a href="index.php?page=logout">Déconnexion</a> |
-        <?php endif; ?>
-        <a href="index.php?page=admin">Espace Administrateur</a>
+    <h1 class="titre">
+        <?php
+        if (isset($_SESSION['prenom'])) {
+            echo 'Bienvenue, ' . htmlspecialchars($_SESSION['prenom']) . ' !';
+        } else {
+            echo 'Bienvenue sur la boutique !';
+        }
+        ?>
+    </h1>
+    <nav class="navbar">
+        <ul>
+            <li> <a href="index.php?page=accueil">Accueil</a></li> |
+            <?php if (!isset($_SESSION['email'])): ?>
+                <li><a href="index.php?page=register">S'inscrire</a></li> |
+                <li><a href="index.php?page=connexion">Se connecter</a></li> |
+            <?php else: ?>
+                <li><a href="index.php?page=logout">Déconnexion</a></li> |
+            <?php endif; ?>
+            <li><a href="index.php?page=admin">Espace Administrateur</a></li>
+        </ul>
     </nav>
 
 </header>
