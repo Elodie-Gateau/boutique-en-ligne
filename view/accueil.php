0861dd2f41f9b48ff8a6c8dd22618e0b2f11a1ce
FabThms <thms.fab44@gmail.com>
2025-07-10 16:49:46 +0200

    ajout mineur html

--- view/accueil.php
+++ view/accueil.php
@@ -9,14 +9,23 @@
 </h1> -->
 
 
+
 <section class="products">
 
-    <div class="card">
+    <div class="cards hero">
         <h2>Liste des produits</h2>
         <?php
 
         $produits = ProduitsController::listProducts();
         require 'liste-produits.php'; ?>
+    </div>
+
+    <div class="main content">
+        <h2> Main content</h2>
+    </div>
+
+    <div class="contacts">
+        <h2>Contatcs</h2>
     </div>
 
 </section>
