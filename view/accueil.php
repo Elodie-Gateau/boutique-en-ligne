c1eb8d7be87da255166a841a78ade208fe9de875
FabThms <thms.fab44@gmail.com>
2025-07-10 14:30:26 +0200

    ajout du style CSS

--- view/accueil.php
+++ view/accueil.php
@@ -1,1 +1,9 @@
-require_once 'include/header.php';
+   <h1 class="titre">
+       <?php
+        if (isset($_SESSION['prenom'])) {
+            echo 'Bienvenue, ' . htmlspecialchars($_SESSION['prenom']) . ' !';
+        } else {
+            echo 'Bienvenue sur la boutique !';
+        }
+        ?>
+   </h1>
