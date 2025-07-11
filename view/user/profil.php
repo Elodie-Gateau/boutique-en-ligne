53be0786b6cc7279c2a06d27f56efaa703b3b858
VERTUEUX Maxime <vertueuxmaxime@gmail.com>
2025-07-11 15:37:13 +0200

    profil

--- view/user/profil.php
+++ view/user/profil.php
@@ -1,15 +1,15 @@
 <section class="profil-user">
-    div class="register">
-    <h2 class="register__title">Mon Profil</h2>
-    <ul class="register__form">
-        <li class="register__label"><strong>Nom :</strong> <?= e($_SESSION['nom'] ?? 'Non défini') ?></li>
-        <li class="register__label"><strong>Prénom :</strong> <?= e($_SESSION['prenom'] ?? 'Non défini') ?></li>
-        <li class="register__label"><strong>Email :</strong> <?= e($_SESSION['email'] ?? 'Non défini') ?></li>
-    </ul>
+    <div class="register">
+        <h2 class="register__title">Mon Profil</h2>
+        <ul class="register__form">
+            <li class="register__label"><strong>Nom :</strong> <?= e($_SESSION['nom'] ?? 'Non défini') ?></li>
+            <li class="register__label"><strong>Prénom :</strong> <?= e($_SESSION['prenom'] ?? 'Non défini') ?></li>
+            <li class="register__label"><strong>Email :</strong> <?= e($_SESSION['email'] ?? 'Non défini') ?></li>
+        </ul>
 
-    <a href="index.php?page=modifierProfil">
-        <button class="register__submit">Modifier mes informations</button>
-    </a>
+        <a href="index.php?page=modifierProfil">
+            <button class="register__submit">Modifier mes informations</button>
+        </a>
     </div>
 </section>
 
